859deb28c909dce195097a2b77398d8393fe4a75
AbstDast <52092402+AbstDast@users.noreply.github.com>
2020-01-16 15:13:21 -0500

    Create items.sql

---
+++ items.sql
@@ -0,0 +1,14 @@
+CREATE TABLE items (id INTEGER PRIMARY KEY, name TEXT);
+
+INSERT INTO items VALUES (1, "Dirt");
+INSERT INTO items VALUES (2, "Soil");
+INSERT INTO items VALUES (3, "Heavy Soil");
+INSERT INTO items VALUES (4, "Rock");
+INSERT INTO items VALUES (5, "Chiseled Rock");
+INSERT INTO items VALUES (6, "Sand");
+INSERT INTO items VALUES (7, "Alexandrite");
+INSERT INTO items VALUES (8, "Tanzanite");
+INSERT INTO items VALUES (9, "Diamond");
+INSERT INTO items VALUES (10, "Rubious Gem");
+INSERT INTO items VALUES (11, "Golden Ore");
+
